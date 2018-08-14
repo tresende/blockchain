bf015ba135493fae53dc07b3e9ed0c1d5f90a73c
Thiago Resende <thiago.gcresende@gmail.com>
2018-08-13 22:22:35 -0300

    format

--- eleicoesonline/blockchain-api/sawtooth/infra.js
+++ eleicoesonline/blockchain-api/sawtooth/infra.js
@@ -1,16 +1,16 @@
-const {createHash} = require('crypto')
-const {protobuf} = require('sawtooth-sdk')
-const {createContext, CryptoFactory} = require('sawtooth-sdk/signing')
-const {Secp256k1PrivateKey} = require('sawtooth-sdk/signing/secp256k1')
+const { createHash } = require('crypto')
+const { protobuf } = require('sawtooth-sdk')
+const { createContext, CryptoFactory } = require('sawtooth-sdk/signing')
+const { Secp256k1PrivateKey } = require('sawtooth-sdk/signing/secp256k1')
 const request = require('request');
 
-function handlerInfo(){
+function handlerInfo() {
   const familyName = 'onlinevoting';
   return {
-            prefix : getAddress(familyName, 6),
-            family : familyName,
-            version :'0.0.1'
-        };
+    prefix: getAddress(familyName, 6),
+    family: familyName,
+    version: '0.0.1'
+  };
 }
 
 function getAddress(key, length) {
@@ -18,35 +18,35 @@ function getAddress(key, length) {
 }
 
 function calculateVoteAddress(payload) {
-  return handlerInfo().prefix + getAddress(payload.ellectionName,20) + getAddress(payload.userNumber,20) + getAddress(payload.address,24);
+  return handlerInfo().prefix + getAddress(payload.ellectionName, 20) + getAddress(payload.userNumber, 20) + getAddress(payload.address, 24);
 }
 
-const getAssetAddress = payload => handlerInfo().prefix + getAddress(payload.ellectionName,20) + getAddress(payload.userNumber,20) + getAddress(payload.address,24)
+const getAssetAddress = payload => handlerInfo().prefix + getAddress(payload.ellectionName, 20) + getAddress(payload.userNumber, 20) + getAddress(payload.address, 24)
 
 function sendToSawtoothApi(batchBytes) {
   request({
-      url: 'http://DOCKER:8008/batches?wait',
-      method: 'POST',
-      body: batchBytes,
-      encoding: null,
-      headers: {'Content-Type': 'application/octet-stream'}
-    }, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      } else {
-        const res = new Buffer(response.body, 'base64').toString()
-        console.log('Response: ', res);
-      }
-    })
+    url: 'http://DOCKER:8008/batches?wait',
+    method: 'POST',
+    body: batchBytes,
+    encoding: null,
+    headers: { 'Content-Type': 'application/octet-stream' }
+  }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const res = new Buffer(response.body, 'base64').toString()
+      console.log('Response: ', res);
+    }
+  })
 }
 
-function buildSawtoothPackage(payloadBytes,privateKey){
+function buildSawtoothPackage(payloadBytes, privateKey) {
 
   const context = createContext('secp256k1');
   const privateKeyInstance = Secp256k1PrivateKey.fromHex(privateKey);
   const signer = new CryptoFactory(context).newSigner(privateKeyInstance);
 
-  const {family,version,prefix} = handlerInfo();
+  const { family, version, prefix } = handlerInfo();
 
   const transactionHeaderBytes = protobuf.TransactionHeader.encode({
     familyName: family,
@@ -61,29 +61,29 @@ function buildSawtoothPackage(payloadBytes,privateKey){
   const signature = signer.sign(transactionHeaderBytes);
 
   const transaction = protobuf.Transaction.create({
-      header: transactionHeaderBytes,
-      headerSignature: signature,
-      payload: payloadBytes
+    header: transactionHeaderBytes,
+    headerSignature: signature,
+    payload: payloadBytes
   });
 
   const batchHeaderBytes = protobuf.BatchHeader.encode({
-      signerPublicKey: signer.getPublicKey().asHex(),
-      transactionIds: [transaction.headerSignature],
+    signerPublicKey: signer.getPublicKey().asHex(),
+    transactionIds: [transaction.headerSignature],
   }).finish();
 
   const batchSignature = signer.sign(batchHeaderBytes);
 
   const batch = protobuf.Batch.create({
-      header: batchHeaderBytes,
-      headerSignature: batchSignature,
-      transactions: [transaction]
+    header: batchHeaderBytes,
+    headerSignature: batchSignature,
+    transactions: [transaction]
   });
 
   const batchBytes = protobuf.BatchList.encode({
-      batches: [batch]
+    batches: [batch]
   }).finish();
 
   return batchBytes;
 }
 
-module.exports = { buildSawtoothPackage,sendToSawtoothApi,handlerInfo,calculateVoteAddress}
+module.exports = { buildSawtoothPackage, sendToSawtoothApi, handlerInfo, calculateVoteAddress }
--- eleicoesonline/blockchain-api/sawtooth/voteHandler.js
+++ eleicoesonline/blockchain-api/sawtooth/voteHandler.js
@@ -2,8 +2,8 @@
 
 const { createHash } = require('crypto')
 const { TransactionHandler } = require('sawtooth-sdk/processor/handler')
-const {Decoder} = require('cbor')
-const {calculateVoteAddress,handlerInfo} = require('./infra');
+const { Decoder } = require('cbor')
+const { calculateVoteAddress, handlerInfo } = require('./infra');
 
 // Encoding helpers and constants
 const getAddress = (key, length = 64) => {
@@ -13,22 +13,22 @@ const getAddress = (key, length = 64) => {
 const encode = obj => Buffer.from(JSON.stringify(obj, Object.keys(obj).sort()))
 
 class VoteNHandler extends TransactionHandler {
-  constructor () {
+  constructor() {
     console.log('Iniciando smart contract para votos ')
     const info = handlerInfo();
     super(info.family, [info.version], [info.prefix]);
   }
 
-  apply (txn, context) {
+  apply(txn, context) {
     console.log("chegando uma nova transacao");
 
     const dataDecoded = Decoder.decodeFirstSync(txn.payload);
     const payload = JSON.parse(dataDecoded);
 
     const blockAddress = calculateVoteAddress(payload)
-    const {candidateNumber,ellectionName} = payload;
+    const { candidateNumber, ellectionName } = payload;
     return context.setState({
-      [blockAddress]: encode({candidateNumber,ellectionName})
+      [blockAddress]: encode({ candidateNumber, ellectionName })
     });
   }
 }
